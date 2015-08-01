637309c79debd4ce18d4852de0d8c8160d47a7a9
SShayashi <SShayashi0208@gmail.com>
2015-08-01 20:39:48 +0900

    モーダル画面クラス

---
+++ Classes/TopModal.h
@@ -0,0 +1,14 @@
+//
+//  TopModal.h
+//  SyuraProject
+//
+//  Created by 林　真史 on 2015/08/01.
+//
+//
+
+#ifndef __SyuraProject__TopModal__
+#define __SyuraProject__TopModal__
+
+#include <stdio.h>
+
+#endif /* defined(__SyuraProject__TopModal__) */
