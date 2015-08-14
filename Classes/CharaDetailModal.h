313836ef2dbf9334cd137ea566b857c7da746920
SShayashi <SShayashi0208@gmail.com>
2015-08-14 16:38:36 +0900

    モーダル画面のテンプレートを追加

--- Classes/CharaDetailModal.h
+++ Classes/CharaDetailModal.h
@@ -10,5 +10,20 @@
 #define __SyuraProject__CharaDetailModal__
 
 #include <stdio.h>
+#include "cocosGUI.h"
+class CharaDetailModal : public cocos2d::Layer
+{
+    
+public:
+    
+    virtual bool init();
+    
+    void menuCloseCallback(Ref* pSender);
+    
+    void pushMenu01(Ref *pSender);
+    
+    // create()を使えるようにしている。
+    CREATE_FUNC(CharaDetailModal);
+};
 
 #endif /* defined(__SyuraProject__CharaDetailModal__) */
--- Classes/ComicListModal.h
+++ Classes/ComicListModal.h
@@ -10,5 +10,21 @@
 #define __SyuraProject__ComicListModal__
 
 #include <stdio.h>
+#include "cocosGUI.h"
+class CharaDetailModal : public cocos2d::Layer
+{
+    
+public:
+    
+    virtual bool init();
+    
+    void menuCloseCallback(Ref* pSender);
+    
+    void pushMenu01(Ref *pSender);
+    
+    // create()を使えるようにしている。
+    CREATE_FUNC(CharaDetailModal);
+};
+
 
 #endif /* defined(__SyuraProject__ComicListModal__) */
