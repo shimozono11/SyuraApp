56c931b93e3993a0e0bd9397a716da48487bd4cf
SShayashi <SShayashi0208@gmail.com>
2015-08-19 19:13:34 +0900

    numbers.pngを追加と制限時間を25秒に変更

---
+++ Classes/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  SyuraProject
+//
+//  Created by 林　真史 on 2015/08/15.
+//
+//
+
+#include "test.h"
---
+++ Classes/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  SyuraProject
+//
+//  Created by 林　真史 on 2015/08/15.
+//
+//
+
+#ifndef __SyuraProject__test__
+#define __SyuraProject__test__
+
+#include <stdio.h>
+
+#endif /* defined(__SyuraProject__test__) */
